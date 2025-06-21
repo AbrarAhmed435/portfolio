7a72464b19bd0a0979909386c51fc6f4137c9e40
Abrar ul Riyaz <abrar_2022bite057@nitsri.ac.in>
2025-06-21 10:29:16 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
 import { useState } from 'react'
+import { Route, Routes } from "react-router-dom";
 import Navbar from './Components/Navbar/Navbar'
+import Home from './Home';
+import About from './About';
+import Contact from './Contact';
+import Others from './Others';
+import Projects from './Projects';
+
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
-      <Navbar/>
+       <Navbar/>
+       <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/contact' element={<Contact/>}/>
+      <Route path='/projects' element={<Projects/>}/>
+      <Route path='/others' element={<Others/>}/>
+</Routes>
+     
     </div>
   )
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,15 +1,17 @@
 import './Navbar.css'
+import { NavLink } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 export default function Navbar() {
   return (
     <div className="navbar">
       <img src="../public/logo.png" alt="logo" /> 
       <ul className="nav-menu">
-        <li>Home</li>
-        <li>About</li>
-        <li>Projects</li>
-        <li>Contact</li>
-        <li>Other</li>
+        <li><Link to='/' className='navlinks'>Home</Link></li>
+        <li><Link to='/about' className='navlinks'>About</Link></li>
+        <li><Link to='/projects' className='navlinks'>Projects</Link></li>
+        <li><Link to='/contact' className='navlinks'>Contact</Link></li>
+        <li><Link to='/others' className='navlinks'>Others</Link></li>
       </ul>
       <button className='nav-connect'>Connect With Me</button>
     </div>
