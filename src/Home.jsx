fe1075da209b81c80f2dd6336b0943b983c95f1d
Abrar ul Riyaz <abrar_2022bite057@nitsri.ac.in>
2025-06-27 22:46:03 +0530

    update

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,12 +1,19 @@
+import { FaHistory } from "react-icons/fa";
+
 export default function Home(){
     return (
         <div className="Home">
             <div className="outer-container">
             <div className="container">
                 <img src="../public/passport.jpg" alt=""  className="passport"/>
-                <div className="box"><p style={{fontSize:'3rem'}}>Hi👋, I am Abrar<br/> IT UNDERGRADUATE</p></div>
+                <div className="box"><p style={{fontSize:'2rem'}}>Hi👋, I am Abrar<br/> IT UNDERGRADUATE</p></div>
 
             </div>
+            
+            </div>
+            <div className="history">
+                <h3 className="history-title">HISTORY <FaHistory /></h3>
+
             </div>
         </div>
     )
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,19 +1,33 @@
-import './Navbar.css'
-import { NavLink } from 'react-router-dom'
-
+import './Navbar.css';
+import { NavLink } from 'react-router-dom';
+import { useState } from 'react';
+import logo from '/logo.png'; // Use logo from public folder or src
 
 export default function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => setMenuOpen(!menuOpen);
+
   return (
     <div className="navbar">
-      <img src="../public/logo.png" alt="logo" /> 
-      <ul className="nav-menu">
+      <img src={logo} alt="logo" />
+
+      {/* Hamburger for mobile */}
+      <div className="hamburger" onClick={toggleMenu}>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+
+      <ul className={`nav-menu ${menuOpen ? 'active' : ''}`}>
         <li><NavLink to='/' className='navlinks'>Home</NavLink></li>
         <li><NavLink to='/about' className='navlinks'>About</NavLink></li>
         <li><NavLink to='/projects' className='navlinks'>Projects</NavLink></li>
         <li><NavLink to='/contact' className='navlinks'>Contact</NavLink></li>
         <li><NavLink to='/others' className='navlinks'>Others</NavLink></li>
       </ul>
+
       <button className='nav-connect'>Connect With Me</button>
     </div>
-  )
+  );
 }
